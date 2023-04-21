3c3fb83d283592b7e138fbcf643c38c6f910d3cc
Shrenik Munot <shrenik.2407@gmail.com>
2023-04-21 09:55:20 +0530

    All saved

---
+++ ReverseArray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array:");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        System.out.println("Enter array elements :");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+            sc.nextLine();
+        }
+
+        System.out.println(" The array in reverse order :");
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
